101a0f4ce9d7947e8bba381d2935d655bfa15803
Joseph Abbey <joseph@abbey1.org.uk>
2023-02-28 18:21:18 +0000

    Do not register a service worker on localhost

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ window.addEventListener('popstate', async () => {
   }
 });
 
+/** Cleans the page for partial replacement. */
 export function clean() {
   buttons.forEach((b) => b.remove());
   buttons = [];
@@ -303,42 +304,44 @@ document
     }
   });
 
-registerServiceWorker();
+if (location.hostname !== 'localhost') {
+  registerServiceWorker();
 
-let deferredPrompt;
+  let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
 
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
 
-  // Update UI to notify the user they can add to home screen
-  addButton(
-    'install_btn',
-    // @ts-ignore
-    function (e) {
-      // Hide our user interface that shows our A2HS button
-      this.style.display = 'none';
+    // Update UI to notify the user they can add to home screen
+    addButton(
+      'install_btn',
+      // @ts-ignore
+      function (e) {
+        // Hide our user interface that shows our A2HS button
+        this.style.display = 'none';
 
-      // Show the prompt
-      deferredPrompt.prompt();
+        // Show the prompt
+        deferredPrompt.prompt();
 
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then(
-        (/** @type {{ outcome: string; }} */ choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then(
+          (/** @type {{ outcome: string; }} */ choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+              console.log('User accepted the A2HS prompt');
+            } else {
+              console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
           }
-          deferredPrompt = null;
-        }
-      );
-    },
-    'Install',
-    'Install',
-    'install_desktop'
-  );
-});
+        );
+      },
+      'Install',
+      'Install',
+      'install_desktop'
+    );
+  });
+}
